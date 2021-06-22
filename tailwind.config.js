98de7fdb4ab403895637cb21365e632bb3597ca4
Marcus <marcus.veres@gmail.com>
2021-06-22 12:46:59 -0400

    Updates npm run watch command (adds purge and CLI watch).

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    "*/**/*.html"
+  ],
+  darkMode: 'class', // false || 'media' || 'class'
   theme: {
-    extend: {},
+    debugScreens: {
+      position: ['top', 'left'],
+    },
+    extend: {
+      colors : {
+        'cornflower':'#1e75bb',
+        'sneaky-blue':'#0e2b49',
+        'social-email':'#6567A5',
+        'social-facebook':'#3b5998',
+        'social-instagram':'#517fa4',
+        'social-pinterest':'#cb2027',
+        'social-twitter':'#00aced',
+      },
+      fontFamily: {
+        // 'orelega': ['"Orelega One"', 'cursive']
+        'raleway': ['"Raleway"', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ]
 }
